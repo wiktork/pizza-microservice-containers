e425672666556b1a40166c56e29287c8a02c79da
Catherine Wang <cawa@microsoft.com>
2022-09-12 10:25:21 -0700

    first commit

---
+++ PizzaOrderProcessor/PizzaOrder.cs
@@ -0,0 +1,23 @@
+﻿namespace PizzaOrderProcessor
+{
+    public class Pizza
+    {
+        public string? Name { get; set; }
+
+        public double ? Price { get; set; }
+
+        public int Count { get; set; }
+    }
+
+
+
+    public class PizzaOrder
+    { 
+
+        public IEnumerable<Pizza>? TotalOrder { get; set; }
+
+        public int ID { get; set; }
+
+        public string ?status { get; set; }
+    }
+}
---
+++ PizzaWeb/app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+//TODO: add Dapr integration for sending order and price in req.body to event broker
+app.post('/submitOrder', function(req, res){
+    var cart = JSON.parse(req.body["cart"]);
+
+    cartToSend = [];
+
+    cart.forEach(element => {
+        cartToSend.push({"name":element.name, "price": element.price, "count": element.count});
+    });
+
+    console.log(JSON.parse(req.body["cart"]));
+    //TODO: send JSON.stringify(cartToSend) to the pub/sub event bus
+    console.log(JSON.stringify(cartToSend));
+    res.sendStatus(200);
+});
+
+module.exports = app;
